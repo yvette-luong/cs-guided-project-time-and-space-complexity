949ab46cb21d997179393d76315e9af8118e2a34
yvette-luong <63530290+yvette-luong@users.noreply.github.com>
2020-12-02 14:56:09 -0800

    Update demonstration_02.py

--- src/demonstration_02.py
+++ src/demonstration_02.py
@@ -11,4 +11,34 @@ Examples:
 """
 def single_number(nums):
     # Your code here
+    s = set()
+# how could we save data to write an implementation is more efficient?
+# for any given number, we'd like to know if we've seen this number already 
+# can we somehow save each number we've seen in such a way that we see 
+# the next copy of it, we can access this quickly 
 
+# loop through nums
+    for num in nums: # 0(n)
+    # check if the num in our set
+    # if the num in our set then
+        if num in s: # 0(1)
+            s.remove(num)
+    # if it isn't 
+        else:
+        #  add it to the set
+            s.add(num)
+
+    # the only thing left in the set should be our odd-number-out 
+    # the only way to grab the odd-element-out from our set is to 
+    # turn it into a list and access it via its index
+    return s.pop() # O(1) since our set should always only have 1 element 
+
+      # O(n^2)
+    # we could use nested for loops to check 
+    # if every element has another copy of itself 
+    # in the list 
+        # the odd-number-out will not find a copy of itself 
+        # so that would be our answer 
+
+
+        
